537c23e1e8896ee39722d6fe2c533f93c230f9df
Vladislav <vladlipchey05@gmail.com>
2024-04-22 11:55:21 +0300

    add depencies libs

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Alert, Button, SafeAreaView, StyleSheet, Text, View , Image, FlatList} from 'react-native';
+import { useState } from 'react';
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.main}>
+      <Text>Template</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  main:{
+    textAlign:'center'
   },
 });
